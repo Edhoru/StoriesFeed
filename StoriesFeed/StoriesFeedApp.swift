34f421bf64301176a99e2ffd5a950172dd7f8b53
Alberto <edhoru@hotmail.com>
2026-06-07 06:36:02 -0600

    Initial Commit

---
+++ StoriesFeed/StoriesFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoriesFeedApp.swift
+//  StoriesFeed
+//
+//  Created by Alberto on 07/06/26.
+//
+
+import SwiftUI
+
+@main
+struct StoriesFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
