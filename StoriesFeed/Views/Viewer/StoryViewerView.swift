3aad52d62cff0a96ff5a1e86aa9ae8d1788a5118
Alberto <edhoru@hotmail.com>
2026-06-07 08:02:52 -0600

    Remove viewer swipe dismiss

--- StoriesFeed/Views/Viewer/StoryViewerView.swift
+++ StoriesFeed/Views/Viewer/StoryViewerView.swift
@@ -2,9 +2,6 @@ import SwiftUI
 
 struct StoryViewerView: View {
     @Bindable var vm: StoryViewerViewModel
-    @State private var dragOffset: CGFloat = 0
-    private let dismissStartThreshold: CGFloat = 24
-    private let dismissThreshold: CGFloat = 100
 
     var body: some View {
         ZStack {
@@ -29,51 +26,10 @@ struct StoryViewerView: View {
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(
             StoryImageLayerView(story: vm.currentStory)
-                .scaleEffect(dragScale)
                 .ignoresSafeArea()
         )
         .background(Color.black.ignoresSafeArea())
-        .offset(y: dragOffset)
-        .simultaneousGesture(dismissDragGesture)
         .onAppear { vm.start() }
         .onDisappear { vm.stop() }
-    }
-
-    private var dismissDragGesture: some Gesture {
-        DragGesture(minimumDistance: dismissStartThreshold)
-            .onChanged { value in
-                guard isDismissDrag(value) else { return }
-                dragOffset = value.translation.height
-                if !vm.isPaused { vm.pause() }
-            }
-            .onEnded { value in
-                guard isDismissDrag(value) else {
-                    if dragOffset != 0 {
-                        withAnimation(.spring(response: 0.35, dampingFraction: 0.7)) {
-                            dragOffset = 0
-                        }
-                    }
-                    return
-                }
-
-                if value.translation.height > dismissThreshold {
-                    withAnimation(.easeIn(duration: 0.2)) { dragOffset = 1000 }
-                    vm.onDismiss?()
-                } else {
-                    withAnimation(.spring(response: 0.35, dampingFraction: 0.7)) {
-                        dragOffset = 0
-                    }
-                    vm.resume()
-                }
-            }
-    }
-
-    private func isDismissDrag(_ value: DragGesture.Value) -> Bool {
-        value.translation.height > dismissStartThreshold
-            && value.translation.height > abs(value.translation.width)
-    }
-
-    private var dragScale: CGFloat {
-        max(0.88, 1.0 - dragOffset / 1400)
     }
 }
